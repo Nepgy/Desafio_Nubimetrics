dbb6707e468515ce0dd88aceeb10ae5879c3ae85
Federico Lucero <Federico.lu0@gmail.com>
2021-05-11 11:09:37 -0300

    Cambio de formato
    Se cambio los nombres de algunas variables dentro de la resolucion de los desafios

--- Desafio 04/4_desafio.py
+++ Desafio 04/4_desafio.py
@@ -2,26 +2,26 @@ import pandas
 import pyarrow
 
 #Abrimos el archivo sales.parquet
-sales_tabla = pandas.read_parquet('Desafio_Nubimetrics\Desafio 4\Dependencias\sales.parquet', engine='pyarrow')
+sales_df = pandas.read_parquet('Desafio_Nubimetrics\Desafio 4\Dependencias\sales.parquet', engine='pyarrow')
 #Abrimos el archivo currenciesClean.csv
-currencies_tabla = pandas.read_csv("Desafio_Nubimetrics\Desafio 4\Dependencias\currenciesClean.csv", "r", delimiter = ",")
+currencies_df = pandas.read_csv("Desafio_Nubimetrics\Desafio 4\Dependencias\currenciesClean.csv", "r", delimiter = ",")
 
 #Renombramos las columnas para hacer el merge 
-currencies_tabla.rename(columns = {'currencyID' : 'currency_id', 'datetrack' : 'date'}, inplace = True)
+currencies_df.rename(columns = {'currencyID' : 'currency_id', 'datetrack' : 'date'}, inplace = True)
 
 #Tomamos las columnas que nos interesa 
-currencies_tabla_sub = currencies_tabla[['currency_id', 'dolarToLocal']]
+currencies_df_sub = currencies_df[['currency_id', 'dolarToLocal']]
 
 
 #Unimos los dataframes en el campo 'currency_id'
-Sales_tabla_full = pandas.merge(sales_tabla ,currencies_tabla_sub, on= 'currency_id', how='left')
+Sales_df_full = pandas.merge(sales_df ,currencies_df_sub, on= 'currency_id', how='left')
 
 #Cantidad de elementos en el campo item_id
-#Cant_items = Sales_tabla_full.item_id.count()
+#Cant_items = Sales_df_full.item_id.count()
 #print(Cant_items)
 
 #Item más vendido
-#max_ventas = Sales_tabla_full[Sales_tabla_full.sales == Sales_tabla_full['sales'].max()]
+#max_ventas = Sales_df_full[Sales_df_full.sales == Sales_df_full['sales'].max()]
 #print(max_ventas.item_id)
 
-Sales_tabla_full.to_parquet('Sales_DF.parquet')	
+Sales_df_full.to_parquet('Sales_DF.parquet')	
